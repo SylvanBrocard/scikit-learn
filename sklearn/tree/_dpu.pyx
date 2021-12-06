8ff3c314bced90eb1bb2080fff9eb0fe1c08987d
SylvanBrocard <sylvan.brocard@gmail.com>
2021-12-06 11:05:21 +0100

    first working call to SDK

---
+++ sklearn/tree/_dpu.pyx
@@ -0,0 +1,16 @@
+cimport numpy as np
+
+ctypedef np.npy_uint32 UINT32_t
+
+cdef extern from "_dpu_c.c":
+   void print_n_clusters_c()
+   void allocate(UINT32_t *ndpu)
+
+def print_n_clusters():
+    print_n_clusters_c()
+
+def test_allocate():
+    cdef UINT32_t ndpu
+    cdef UINT32_t *ndpu_pointer = &ndpu
+    allocate(ndpu_pointer)
+    print(f"here too NDPU = {ndpu}")
